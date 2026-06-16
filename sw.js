025ee078ce4eaf4e1e5fa3c0830f35e61e9f855d
yxm0825 <149454842+yxm0825@users.noreply.github.com>
2026-06-16 20:38:22 +0800

    v8 - 单日复制加日期 + 默认客户名清空 + 截图字体加深

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'veggie-ledger-v7';
+﻿const CACHE_NAME = 'veggie-ledger-v8';
 const ASSETS = [
   '.',
   'index.html',
@@ -53,4 +53,5 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
