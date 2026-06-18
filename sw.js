54931ea54a2a93d214c7f8b6dd5b60e0395bf824
yxm0825 <149454842+yxm0825@users.noreply.github.com>
2026-06-19 02:36:48 +0800

    v31 - 回退简单密码页 版本检查方案放弃

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'veggie-ledger-v30';
+﻿const CACHE_NAME = 'veggie-ledger-v31';
 const ASSETS = [
   '.',
   'index.html',
@@ -55,6 +55,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
